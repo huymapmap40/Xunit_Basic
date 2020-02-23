bfac0872e3e7c47293eafcfbca658a3ea45037e4
Quoc Huy Nguyen <huyquocnguyen532@gmail.com>
2020-02-23 22:35:36 +0700

    add new

---
+++ XunitPractice/HandleContext/HandlingFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XunitPractice.HandleContext
+{
+    public class HandlingFixture : IDisposable
+    {
+        public HandlingFixture()
+        {
+            //... Constructor to begin each test
+            Console.WriteLine("Before the test");
+        }
+
+        public void Dispose()
+        {
+            //...Method cleanup the test
+            Console.WriteLine("After the test");
+        }
+    }
+}
---
+++ XunitPractice/TestCaseSample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using XunitPractice.HandleContext;
+
+namespace XunitPractice
+{
+    public class TestCaseSample: IClassFixture<HandlingFixture>
+    {
+        HandlingFixture handlingFixture;
+
+        public TestCaseSample(HandlingFixture handlingFixture)
+        {
+            this.handlingFixture = handlingFixture;
+        }
+
+        [Theory]
+        [Trait("Sample", "TEST_1234")]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void TestNumberIsOdd(int number)
+        {
+            Assert.True(IsOdd(number));
+        }
+
+        [Theory]
+        [Trait("Sample","TEST_5678")]
+        [InlineData(1, 2, 3)]
+        [InlineData(4, 5, 6)]
+        [InlineData(7, 8, 15)]
+        public void TestSum(int num1, int num2, int sum)
+        {
+            Assert.Equal(num1 + num2, sum);
+        }
+
+        private bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
